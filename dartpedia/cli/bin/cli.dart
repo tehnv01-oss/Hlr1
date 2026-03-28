adb6a3621889a74a8473d8c9a2444bc92e270b45
Viktor4lvl/lr1.git <vitya.podolskiy.01@mail.ru>
2026-03-28 10:40:33 +0300

    Обновить cli.dart для использования нового CommandRunner

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,8 +1,8 @@
-import 'dart:io';
-import 'package:http/http.dart' as http;
 import 'package:command_runner/command_runner.dart';
 
-void main(List<String> arguments) async { // main is now async and awaits the runner
-  var runner = CommandRunner(); // Create an instance of your new CommandRunner
-  await runner.run(arguments); // Call its run method, awaiting its Future<void>
+const version = '0.0.1';
+
+void main(List<String> arguments) {
+  var commandRunner = CommandRunner()..addCommand(HelpCommand());
+  commandRunner.run(arguments);
 }
