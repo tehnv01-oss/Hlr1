2c92ad2fa511f2ca3414fc7e95f128923d6b1b15
Viktor4lvl/lr1.git <vitya.podolskiy.01@mail.ru>
2026-03-28 09:48:37 +0300

    Реализовал версии и команды помощи, заменив код в Main

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,9 +1,17 @@
 const version = '0.0.1';
 
 void main(List<String> arguments) {
-  if (arguments.isEmpty) {
-    print('Hello, Dart!');
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage(); // Change this from 'Hello, Dart!'
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
+  } else {
+    printUsage(); // Catch-all for any unrecognized command.
   }
+}
+
+void printUsage() { // Add this new function
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
+  );
 }
