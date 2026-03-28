6eca188ba1477ed1184a1a2f41e2043c14db3d3a
Viktor4lvl/lr1.git <vitya.podolskiy.01@mail.ru>
2026-03-28 09:53:54 +0300

    Реализовал команду поиска, добавив википедию, изменив там код и проверил поиск в википедии, так же добавил команду dart io

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -2,11 +2,15 @@ const version = '0.0.1';
 
 void main(List<String> arguments) {
   if (arguments.isEmpty || arguments.first == 'help') {
-    printUsage(); // Change this from 'Hello, Dart!'
+    printUsage();
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    // Add this new block:
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
   } else {
-    printUsage(); // Catch-all for any unrecognized command.
+    printUsage();
   }
 }
 
@@ -14,4 +18,8 @@ void printUsage() { // Add this new function
   print(
     "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
   );
+}
+
+void searchWikipedia(List<String>? arguments) { // Add this new function and add ? to arguments type
+  print('searchWikipedia received arguments: $arguments');
 }
