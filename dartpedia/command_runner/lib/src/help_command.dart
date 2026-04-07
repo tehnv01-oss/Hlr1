9acbbf5e30db08f311608bb88d5e8335829144da
Viktor4lvl/lr1.git <vitya.podolskiy.01@mail.ru>
2026-04-07 14:02:25 +0300

    Улучшить результатHelpCommand

--- dartpedia/command_runner/lib/src/help_command.dart
+++ dartpedia/command_runner/lib/src/help_command.dart
@@ -1,6 +1,10 @@
 import 'dart:async';
 
-import 'arguments.dart';
+import 'package:command_runner/command_runner.dart';
+
+import 'console.dart';
+import 'exceptions.dart';
+
 
 // Prints program and argument usage.
 //
@@ -35,13 +39,59 @@ class HelpCommand extends Command {
   @override
   String? get help => 'Prints this usage information';
 
-  @override
-  FutureOr<Object?> run(ArgResults args) async {
-    var usage = runner.usage;
-    for (var command in runner.commands) {
-      usage += '\n ${command.usage}';
+@override
+FutureOr<String> run(ArgResults args) async {
+  final buffer = StringBuffer();
+  buffer.writeln(runner.usage.titleText);
+
+  if (args.flag('verbose')) {
+    for (var cmd in runner.commands) {
+      buffer.write(_renderCommandVerbose(cmd));
     }
 
-    return usage;
+    return buffer.toString();
   }
+
+  if (args.hasOption('command')) {
+    var (:option, :input) = args.getOption('command');
+
+    var cmd = runner.commands.firstWhere(
+      (command) => command.name == input,
+      orElse: () {
+        throw ArgumentException(
+          'Input ${args.commandArg} is not a known command.',
+        );
+      },
+    );
+
+    return _renderCommandVerbose(cmd);
+  }
+
+  // Verbose is false and no arg was passed in, so print basic usage.
+  for (var command in runner.commands) {
+    buffer.writeln(command.usage);
+  }
+
+  return buffer.toString();
+}
+
+
+
+}
+
+String _renderCommandVerbose(Command cmd) {
+  final indent = ' ' * 10;
+  final buffer = StringBuffer();
+  buffer.writeln(cmd.usage.instructionText); //abbr, name: description
+  buffer.writeln('$indent ${cmd.help}');
+  if (cmd.valueHelp != null) {
+    buffer.writeln(
+      '$indent [Argument] Required? ${cmd.requiresArgument}, Type: ${cmd.valueHelp}, Default: ${cmd.defaultValue ?? 'none'}',
+    );
+  }
+  buffer.writeln('$indent Options:');
+  for (var option in cmd.options) {
+    buffer.writeln('$indent ${option.usage}');
+  }
+  return buffer.toString();
 }
